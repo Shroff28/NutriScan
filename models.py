175f4b2ae7835da9cf01faadbbe4921ac0dd559e
Monika Govindaraj <135772666+govindam23@users.noreply.github.com>
2024-02-21 13:42:26 -0500

    Add files via upload
    Model for login page

---
+++ models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    # Add more fields as needed
+
