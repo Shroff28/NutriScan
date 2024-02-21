87800193ce650410a0d95987a7023c31381ab4cd
Monika Govindaraj <135772666+govindam23@users.noreply.github.com>
2024-02-21 14:01:48 -0500

    Update models.py
    Added initial model for login page

--- djangoProject/djangoProject/models.py
+++ djangoProject/djangoProject/models.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class User:
+class User(models.Model):
     #TODO: Gargi and monica
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    # Add more fields as needed
 
 class Restaurant:
     #TODO: Astha
