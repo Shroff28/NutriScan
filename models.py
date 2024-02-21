b9b50fb19b2bf50ee9a472ea4ba6ccc739d8254c
Astha <135056448+ABUoW@users.noreply.github.com>
2024-02-21 17:22:07 -0500

    Add files via upload

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    # Add more fields as needed
+class Cuisine(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 
