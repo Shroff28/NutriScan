0ac6e2eb20900a21fa7d73431a9e8b7f21aae05d
Gargi Purandare <135798162+gargirp@users.noreply.github.com>
2024-02-21 12:03:08 -0700

    Update models.py
    Updated UserProfile model for User Setting Page

--- djangoProject/djangoProject/models.py
+++ djangoProject/djangoProject/models.py
@@ -8,6 +8,16 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     # Add more fields as needed
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    bio = models.TextField(blank=True)
+    favorite_food = models.CharField(max_length=100, blank=True)
+    # additional fields for user settings
+    
+    def __str__(self):
+        return self.user.username
+
 class Restaurant:
     #TODO: Astha
 
