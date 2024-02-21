b4b811c0b9bf8e28996daa472d0a1e7a08b3c92c
Paurav Thakkar <thakka38@uwindsor.ca>
2024-02-21 09:33:46 -0500

    Added basic class names

---
+++ djangoProject/djangoProject/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class User:
+    #TODO: Gargi and monica
+
+class Restaurant:
+    #TODO: Astha
+
+class Menu:
+    # TODO: Dhrumil
+
+class Order:
+    # TODO: Jaydeep
+
+class Payments:
+    #TODO: Vinit
+
+
