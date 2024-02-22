7f9eafbd78bcbd3af46c40e93b22bcb2e5725ca3
vinit-chauhan <chauhanvinit23@gmail.com>
2024-02-21 23:11:51 -0500

    Added model for reviews

--- djangoProject/djangoProject/admin.py
+++ djangoProject/djangoProject/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, UserProfile, Comment
+from .models import User, UserProfile, Review, Comment
 
 # Register your models here.
 admin.site.register(User)
@@ -7,5 +7,6 @@ admin.site.register(User)
 # Registering model for User Setting
 admin.site.register(UserProfile)
 
+admin.site.register(Review)
 admin.site.register(Comment)
 
--- djangoProject/djangoProject/models.py
+++ djangoProject/djangoProject/models.py
@@ -41,8 +41,34 @@ class Payments:
 
 
 class Comment(models.Model):
+    # Short title of the comment
     title = models.CharField(max_length=100)
+    # Detailed comment
     details = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    RATINGS_RANGE = (
+        (1, 'Poor'),
+        (2, 'Bad'),
+        (3, 'Mediocre'),
+        (4, 'Good'),
+        (5, 'Excellent'),
+    )
+
+    # timestamp to track when the review was given
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # reference of the user who made the comment
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    # reference of the restaurant the review was given to
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    # rating given to the restaurant
+    rating = models.IntegerField(default=0, choices=RATINGS_RANGE)
+    # comment associated with the review
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-timestamp"]
