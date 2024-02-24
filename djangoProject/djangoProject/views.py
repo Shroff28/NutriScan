712d43a8144f1cb547bee18a4ef897861e5690fd
Paurav Thakkar <thakka38@uwindsor.ca>
2024-02-23 21:48:22 -0500

    adding 2 filters in views

--- djangoProject/djangoProject/views.py
+++ djangoProject/djangoProject/views.py
@@ -5,3 +5,16 @@ from django.shortcuts import render
 
 from .models import User, UserProfile, Order, Restaurant, Cuisine, Menu, Payments, Review, Comment
 
+def restaurant_list(request):
+    restaurants = Restaurant.objects.all()
+
+    searchValue = "test"
+    # Filter according to name in search
+    filteredRestaurants = restaurants.objects.filter(name__icontains=searchValue)
+
+    #Filter according to ratings
+    ratingValue = 1
+    filteredRestaurants = filteredRestaurants.objects.filter(rating__gte=1)
+
+    # TODO return proper template
+    return '';
