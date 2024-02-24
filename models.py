35d310349feba1da407de001b56841ec77a5bc85
Paurav Thakkar <thakka38@uwindsor.ca>
2024-02-23 21:25:51 -0500

    resolving merge conflicts

--- models.py
+++ models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    # Add more fields as needed
+
 class Cuisine(models.Model):
     name = models.CharField(max_length=100)
 
