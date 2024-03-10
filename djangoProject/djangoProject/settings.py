a888b247cab428ca91b59416b7e341afc173f3d2
vinit-chauhan <chauhanvinit23@gmail.com>
2024-03-10 15:25:21 -0400

    Updated settings.py

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoProject',
+    'djstripe',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +118,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJSTRIPE_FOREIGN_KEY_TO_FIELD = "id"
+
