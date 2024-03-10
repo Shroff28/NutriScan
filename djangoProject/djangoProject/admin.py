58e84c7e6db55aacf42f6b130195b19d355d840b
vinit-chauhan <chauhanvinit23@gmail.com>
2024-03-10 16:34:04 -0400

    Added Models to admin for testing

--- djangoProject/djangoProject/admin.py
+++ djangoProject/djangoProject/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, UserProfile, Review, Comment
+from .models import User, UserProfile, Review, Restaurant, Cuisine
 
 # Register your models here.
 admin.site.register(User)
@@ -8,5 +8,6 @@ admin.site.register(User)
 # Registering model for User Setting
 admin.site.register(UserProfile)
 
+admin.site.register(Cuisine)
+admin.site.register(Restaurant)
 admin.site.register(Review)
-admin.site.register(Comment)
