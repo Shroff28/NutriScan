d2963cdea59d35e17feef95213ed0c6c118d606a
Paurav Thakkar <thakka38@uwindsor.ca>
2024-03-14 19:30:07 -0400

    Added Navbar

--- app/views.py
+++ app/views.py
@@ -61,3 +61,7 @@ def user_settings(request):
         profile_form = UserProfileForm(instance=user_profile)
 
     return render(request, 'user_settings.html',{'password_form': password_form, 'profile_form': profile_form, 'user_profile': user_profile})
+
+
+def tempNavbar(request):
+    return render(request, 'navbar.html')
