d25303f498dbf35058585c37dc61ae3270a16a72
purandag <purandag@uwindsor.ca>
2024-03-15 13:28:36 -0600

    updated forms for settings page

--- app/forms.py
+++ app/forms.py
@@ -21,4 +21,4 @@ class UserProfileForm(forms.ModelForm):
     profile_picture = forms.ImageField(required=False)
     class Meta:
         model = UserProfile
-        fields = ('bio', 'favorite_food', 'favorite_restaurant','profile_picture',)
+        fields = ('Name','profile_picture','e-mail','bio', 'favorite_food', 'favorite_restaurant',)
