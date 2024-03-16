306d697faa2cfd46a526c0ff902a5302cc3338ee
Astha <135056448+ABUoW@users.noreply.github.com>
2024-03-16 18:21:27 -0400

    Update models.py

--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class Restaurant(models.Model):
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=20)
-    website = models.URLField(blank=True, null=True)
+    website = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
