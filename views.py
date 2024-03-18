be04057124a71368bf0590b888ff4376ee84916b
Astha <135056448+ABUoW@users.noreply.github.com>
2024-03-18 14:34:33 -0400

    Update views.py

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Restaurant
 
 def restaurant_list(request):
-    restaurants = Restaurant.objects.all()
+    restaurants = Restaurant
     return render(request, 'restaurant_list.html', {'restaurants': restaurants})
 
 def restaurant_detail(request, restaurant_id):
